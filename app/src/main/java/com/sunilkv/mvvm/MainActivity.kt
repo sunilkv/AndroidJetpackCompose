6948dbe0abb7fe7915b4b8a6b9ceb640d9259d70
Sunil KV <sunilkv@Sunils-Mac-mini.local>
2024-03-27 01:34:10 +0530

    counter app

--- app/src/main/java/com/sunilkv/mvvm/MainActivity.kt
+++ app/src/main/java/com/sunilkv/mvvm/MainActivity.kt
@@ -3,6 +3,18 @@ package com.sunilkv.mvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import com.sunilkv.mvvm.ui.theme.MVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +22,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MVVMTheme {
+                var count by remember{
+                    mutableStateOf(0)
+                }
+                // A surface container using the 'background' color from the theme
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = count.toString(), fontSize = 20.sp)
+                    Button(onClick = { count++}) {
+                        Text(text = "Click me $count ")
 
+                    }
+                }
             }
         }
     }
